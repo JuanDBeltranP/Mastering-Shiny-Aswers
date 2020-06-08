0e0c005895c48a30a6726f373c2cfff9e2107b9d
Juan Beltrán <jd.beltran43@gmail.com>
2020-06-08 21:22:56 +0100

    changes in the app

--- app.R
+++ app.R
@@ -4,23 +4,41 @@ library(ggplot2)
 datasets <- data(package = "ggplot2")$results[c(2, 4, 10), "Item"]
 
 
-ui <- fluidPage(theme = shinythemes::shinytheme("superhero"),
-  titlePanel("Central limit theorem"),
-  sidebarLayout(
-    
-    mainPanel(
-      plotOutput("hist")
+ui <- fluidPage(
+  fluidRow(
+    column(4, 
+           "Distribution 1",
+           numericInput("n1", label = "n", value = 1000, min = 1),
+           numericInput("mean1", label = "µ", value = 0, step = 0.1),
+           numericInput("sd1", label = "σ", value = 0.5, min = 0.1, step = 0.1)
     ),
-    sidebarPanel(
-      numericInput("m", "Number of samples:", 2, min = 1, max = 100)
+    column(4, 
+           "Distribution 2",
+           numericInput("n2", label = "n", value = 1000, min = 1),
+           numericInput("mean2", label = "µ", value = 0, step = 0.1),
+           numericInput("sd2", label = "σ", value = 0.5, min = 0.1, step = 0.1)
+    ),
+    column(4,
+           "Histogram",
+           numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
+           sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
     )
+  ),
+  fluidRow(
+    column(9, plotOutput("hist")),
+    column(3, verbatimTextOutput("ttest"))
   )
 )
 
 server <- function(input, output, session) {
+  x1 <- reactive(rnorm(input$n1, input$mean1, input$sd1))
+  x2 <- reactive(rnorm(input$n2, input$mean2, input$sd2))
   output$hist <- renderPlot({
-    means <- replicate(1e4, mean(runif(input$m)))
-    hist(means, breaks = 20)
+        histogram(x1(), x2(), binwidth = input$binwidth, xlim = input$range)
   }, res = 96)
+  
+  output$ttest <- renderText({
+    t_test(x1(), x2())
+  })
 }
 shinyApp(ui, server)
