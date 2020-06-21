844c5d0f0bd3c66cf3d8f9331db823a97dde8ba1
Juan Beltrán <jd.beltran43@gmail.com>
2020-06-21 12:53:15 +0100

    Chanes in the app Chapter 5

--- app.R
+++ app.R
@@ -2,54 +2,54 @@
 library(shiny)
 library(ggplot2)
 library(neiss)
-library(tidyverse)
-datasets <- data(package = "ggplot2")$results[c(2, 4, 10), "Item"]
-injuries %>%
-  filter(trmt_date >= as.Date("2017-01-01"), trmt_date < as.Date("2018-01-01")) %>%
-  semi_join(top_prod, by = "prod1") %>%
-  mutate(age = floor(age), sex = tolower(sex), race = tolower(race)) %>%
-  filter(sex != "unknown") %>%
-  select(trmt_date, age, sex, race, body_part, diag, location, prod_code = prod1, weight, narrative) %>%
-  vroom::vroom_write("neiss/injuries.tsv.gz")
-
+library(magrittr)
+products <- vroom::vroom("neiss/products.tsv")
 injuries <- vroom::vroom("neiss/injuries.tsv.gz")
-injuries
+population <- vroom::vroom("neiss/population.tsv")
+
+count_top <- function(df, var, n = 5) {
+  df %>%
+    mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+    group_by({{ var }}) %>%
+    summarise(n = as.integer(sum(weight)))
+}
 
 ui <- fluidPage(
   fluidRow(
-    column(4, 
-           "Distribution 1",
-           numericInput("n1", label = "n", value = 1000, min = 1),
-           numericInput("mean1", label = "µ", value = 0, step = 0.1),
-           numericInput("sd1", label = "σ", value = 0.5, min = 0.1, step = 0.1)
-    ),
-    column(4, 
-           "Distribution 2",
-           numericInput("n2", label = "n", value = 1000, min = 1),
-           numericInput("mean2", label = "µ", value = 0, step = 0.1),
-           numericInput("sd2", label = "σ", value = 0.5, min = 0.1, step = 0.1)
-    ),
-    column(4,
-           "Histogram",
-           numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
-           sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
+    column(6,
+           selectInput("code", "Product", setNames(products$prod_code, products$title))
     )
   ),
   fluidRow(
-    column(9, plotOutput("hist")),
-    column(3, verbatimTextOutput("ttest"))
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
   )
 )
-
 server <- function(input, output, session) {
-  x1 <- reactive(rnorm(input$n1, input$mean1, input$sd1))
-  x2 <- reactive(rnorm(input$n2, input$mean2, input$sd2))
-  output$hist <- renderPlot({
-        histogram(x1(), x2(), binwidth = input$binwidth, xlim = input$range)
-  }, res = 96)
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
   
-  output$ttest <- renderText({
-    t_test(x1(), x2())
+  output$diag <- renderTable(count_top(selected(), diag), width = "100%")
+  output$body_part <- renderTable(count_top(selected(), body_part), width = "100%")
+  output$location <- renderTable(count_top(selected(), location), width = "100%")
+  
+  
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
   })
+  
+  output$age_sex <- renderPlot({
+    summary() %>%
+      ggplot(aes(age, n, colour = sex)) +
+      geom_line() +
+      labs(y = "Estimated number of injuries")
+  }, res = 96)
 }
+
 shinyApp(ui, server)
--- code_extra.R
+++ code_extra.R
@@ -1,29 +1,8 @@
 library(tidyverse)
-devtools::install_github("hadley/neiss")
+#devtools::install_github("hadley/neiss")
 library(neiss)
 
-top_prod <- injuries %>%
-  filter(trmt_date >= as.Date("2017-01-01"), trmt_date < as.Date("2018-01-01")) %>%
-  count(prod1, sort = TRUE) %>%
-  filter(n > 5 * 365)
-
 injuries %>%
-  filter(trmt_date >= as.Date("2017-01-01"), trmt_date < as.Date("2018-01-01")) %>%
-  semi_join(top_prod, by = "prod1") %>%
-  mutate(age = floor(age), sex = tolower(sex), race = tolower(race)) %>%
-  filter(sex != "unknown") %>%
-  select(trmt_date, age, sex, race, body_part, diag, location, prod_code = prod1, weight, narrative) %>%
-  vroom::vroom_write("neiss/injuries.tsv.gz")
-
-injuries <- vroom::vroom("neiss/injuries.tsv.gz")
-injuries
-#> # A tibble: 255,064 x 10
-#>   trmt_date    age sex   race  body_part diag  location prod_code weight
-#>   <date>     <dbl> <chr> <chr> <chr>     <chr> <chr>        <dbl>  <dbl>
-#> 1 2017-01-01    71 male  white Upper Tr… Cont… Other P…      1807   77.7
-#> 2 2017-01-01    16 male  white Lower Arm Burn… Home           676   77.7
-#> 3 2017-01-01    58 male  white Upper Tr… Cont… Home           649   77.7
-#> 4 2017-01-01    21 male  white Lower Tr… Stra… Home          4076   77.7
-#> 5 2017-01-01    54 male  white Head      Inte… Other P…      1807   77.7
-#> 6 2017-01-01    21 male  white Hand      Frac… Home          1884   77.7
-#> # … with 255,058 more rows, and 1 more variable: narrative <chr>
+  mutate(diag = fct_lump(fct_infreq(diag), n = 5)) %>%
+  group_by(diag) %>%
+  summarise(n = as.integer(sum(weight)))
